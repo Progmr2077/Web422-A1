48cbd31c16135a379eacf879bf00b2b6d67a92f9
jrivera17 <jrivera17@myseneca.ca>
2025-01-18 11:34:11 -0500

    save changes

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Jacob Rivera Student ID: 109641233 Date: January 18, 2025
+* Vercel Link: _______________________________________________________________
+*
+********************************************************************************/
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
