c530a6649455b0728c10a04da70e86931b97009b
jrivera17 <jrivera17@myseneca.ca>
2025-02-08 18:17:07 -0500

    Fixed url

--- public/js/main.js
+++ public/js/main.js
@@ -7,15 +7,11 @@
  * Name: Your Name Student ID: Your ID Date:   
  ********************************************************************************/
 
-require('dotenv').config();
-const MONGODB_CONN_STRING = process.env.MONGODB_CONN_STRING;
-
 let page = 1;
 const perPage = 10;
 
 function loadMovieData(title = null) {
-    // Use environment variable for URL
-    let url = `${MONGODB_CONN_STRING}/api/movies?page=${page}&perPage=${perPage}`;
+    let url = `/api/movies?page=${page}&perPage=${perPage}`;
     if (title) {
         url += `&title=${title}`;
         page = 1;
@@ -43,7 +39,7 @@ function loadMovieData(title = null) {
             document.querySelectorAll("#moviesTable tbody tr").forEach(row => {
                 row.addEventListener("click", () => {
                     const movieId = row.getAttribute("data-id");
-                    fetch(`${MONGODB_CONN_STRING}/api/movies/${movieId}`)
+                    fetch(`/api/movies/${movieId}`)
                         .then(response => response.json())
                         .then(movie => {
                             document.querySelector("#detailsModalLabel").textContent = movie.title;
